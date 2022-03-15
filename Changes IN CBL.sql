3956af089ce06bf56a7e28f65b5bb84838500c5e
Yasir4733 <55761364+Yasir4733@users.noreply.github.com>
2022-03-15 18:50:04 +0500

    Update Changes IN CBL.sql

--- Changes IN CBL.sql
+++ Changes IN CBL.sql
@@ -92,6 +92,8 @@ CREATE TABLE [Essentials].[ParentStyleTemplate]
 (
     ParentStyleTemplateID INT NOT NULL IDENTITY(1,1),
     ParentStyleTemplateDescription VARCHAR(64) NOT NULL,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE()
 
     CONSTRAINT PK_ParentStyleTemplate PRIMARY KEY (ParentStyleTemplateID),
     CONSTRAINT UQ_ParentStyleTemplateDescription UNIQUE (ParentStyleTemplateDescription)
@@ -136,8 +138,16 @@ CONSTRAINT FK_StyleTemplate_ParentStyleTemplate FOREIGN KEY(ParentStyleTemplateI
 
 
 
+
+
+
+
+
 ALTER TABLE  [Essentials].[Machine]
 ADD CONSTRAINT UQ_Machine_MachineID_BoxID UNIQUE (MachineID, BoxID);
+
+
+
 
 ALTER TABLE [Essentials].[ParentStyleTemplate]
 ADD 
@@ -160,3 +170,57 @@ ALTER TABLE [Essentials].[User]
 ADD
 UserImageUrl VARCHAR(2056) DEFAULT NULL,
 UserThumbnailUrl VARCHAR(2056) DEFAULT NULL;
+
+
+
+-- =============================================
+
+USE [ApparalSooperWizer];
+
+
+CREATE TABLE [Essentials].[ParentStyleTemplate]
+(
+    ParentStyleTemplateID INT NOT NULL IDENTITY(1,1),
+    ParentStyleTemplateDescription VARCHAR(64) NOT NULL,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE()
+
+    CONSTRAINT PK_ParentStyleTemplate PRIMARY KEY (ParentStyleTemplateID),
+    CONSTRAINT UQ_ParentStyleTemplateDescription UNIQUE (ParentStyleTemplateDescription)
+
+);
+
+
+CREATE TABLE [Essentials].[ParentStyleTemplateDetail]
+(
+    ParentStyleTemplateDetailID INT NOT NULL IDENTITY(1,1),
+    ParentStyleTemplateID INT NOT NULL,
+    OperationID INT NOT NULL,
+	OperationSequence INT NOT NULL,
+	ScanType VARCHAR(10) NOT NULL,
+	IsFirst BIT NOT NULL DEFAULT 0,
+	IsLast BIT NOT NULL DEFAULT 0,
+	MachineTypeID INT NOT NULL,
+	SMV FLOAT NULL,
+	PieceRate FLOAT NULL,
+	IsCritical bit NOT NULL DEFAULT 0,
+	CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+	UpdatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+	CONSTRAINT PK_ParentStyleTemplateDetail PRIMARY KEY (ParentStyleTemplateDetailID),
+    CONSTRAINT UQ_ParentStyleTemplateDetail_ParentStyleTemplateID_OperationID UNIQUE (ParentStyleTemplateID, OperationID),
+	CONSTRAINT FK_ParentStyleTemplateDetail_Operation FOREIGN KEY (OperationID) REFERENCES [Essentials].[Operation] (OperationID),
+    CONSTRAINT FK_ParentStyleTemplateDetail_MachineType FOREIGN KEY (MachineTypeID) REFERENCES [Essentials].[MachineType](MachineTypeID),
+	CONSTRAINT FK_ParentStyleTemplateDetail_StyleTemplate FOREIGN KEY (ParentStyleTemplateID) REFERENCES [Essentials].[ParentStyleTemplate] (ParentStyleTemplateID)
+
+);
+
+
+
+ALTER TABLE [Essentials].[CutReport]
+ADD StyleTemplateID INT,
+CONSTRAINT FK_CutReport_StyleTemplate FOREIGN KEY(StyleTemplateID) REFERENCES [Essentials].[StyleTemplate](StyleTemplateID);
+
+
+ALTER TABLE [Essentials].[StyleTemplate]
+ADD ParentStyleTemplateID INT,
+CONSTRAINT FK_StyleTemplate_ParentStyleTemplate FOREIGN KEY(ParentStyleTemplateID) REFERENCES [Essentials].[ParentStyleTemplate](ParentStyleTemplateID);
